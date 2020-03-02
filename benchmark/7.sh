b00f4fce843f8e9e9b7d6ecf8ec340e16aad11cb
michalborkowski96 <michalborkowski96@yahoo.pl>
2020-03-02 14:40:35 +0100

    asdf

---
+++ benchmark/7.sh
@@ -0,0 +1,22 @@
+if [ "$#" -ne 2 ]; then
+        echo "Wrong number of arguments, expected 2."
+        exit 1
+fi
+
+source test.sh
+
+test_result=$(perform_test_🍆 7 "$1 $2")
+user_time=$(get_user_time "$test_result")
+used_memory=$(get_used_memory "$test_result")
+#echo "$test_result"
+echo "$user_time"
+echo "$used_memory"
+
+test_result=$(perform_test_c++ 7 "$1 $2")
+user_time=$(get_user_time "$test_result")
+used_memory=$(get_used_memory "$test_result")
+#echo "$test_result"
+echo "$user_time"
+echo "$used_memory"
+
+
